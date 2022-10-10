69849c6ca0c5f11a181af2a7b2ba15177da28e16
nilsonmax <nilsonbenitez@hotmail.com>
2022-10-10 02:59:56 -0500

    instagram Api centrado

--- src/Components/Instagram/Instagram.js
+++ src/Components/Instagram/Instagram.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import '../../SCSS/Instagram.scss'
-import imagen from '../assets/instagram.png'
+// import imagen from '../assets/instagram.png'
+import InstaFeeds from '../Feeds/InstaFeeds'
 
 export const Instagram = () => {
     return (
 
-        <div className='container'>
+        <div className='containerInst'>
             <p className='txtInstagram'>Instagram</p>
             <h2 className='titleInstagram'>#espufi</h2>
-            <img className='container' src={imagen} alt='instagram'/>
+            {/* <img className='container' src={imagen} alt='instagram'/> */}
+            <div class="wrapper">
+                <div class="item1"><InstaFeeds token={process.env.REACT_APP_INS_TOKEN} limit={6} /></div>
+            </div>
+            <div className='block'></div>
         </div>
     )
 }
